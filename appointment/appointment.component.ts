09645538174f91cdc8eee7d4cd04c8867e575682
GitaGurav <guravgitanjalee2002@gmail.com>
2025-02-13 16:16:34 +0530

    Add files via upload

---
+++ appointment/appointment.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { AppointmentService } from '../appointment.service';
+import { Appointment } from '../appointment';
+
+@Component({
+  selector: 'app-appointment',
+  standalone: false,
+  templateUrl: './appointment.component.html',
+  styleUrl: './appointment.component.css'
+})
+export class AppointmentComponent {
+
+searchPatients() {
+throw new Error('Method not implemented.');
+}
+onLogout() {
+throw new Error('Method not implemented.');
+}
+
+appointments:Appointment[]=[];
+searchText: any;
+
+  constructor(private appointmentService:AppointmentService){} 
+
+  ngOnInit():void{
+    this.getAppointments();
+  }
+  getAppointments(){
+    this.appointmentService.getAllAppointments().subscribe(data=>{
+      this.appointments=data;
+    })
+  }
+  delete(id:number){
+    this.appointmentService.deleteAppointment(id).subscribe(data=>{
+      console.log(data);  
+      this.getAppointments();
+    })
+
+  }
+}
